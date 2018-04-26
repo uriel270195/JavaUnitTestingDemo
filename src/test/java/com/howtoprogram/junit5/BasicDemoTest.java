669c08f39c168f2d2a1a0af5d1e0f6e9fd34179d
uriel270195 <uriel_270195@hotmail.com>
2018-04-26 18:18:11 -0500

    Pruebas unitarias en java y ejercicio de cvalculadora

--- src/test/java/com/howtoprogram/junit5/BasicDemoTest.java
+++ src/test/java/com/howtoprogram/junit5/BasicDemoTest.java
@@ -4,16 +4,50 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class BasicDemoTest {
-  private BasicDemo basicDemo;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 
+public class BasicDemoTest {
+  private BasicDemo basicDemo;//instancia
+
+  //@BeforeAll//Ejecuta antes y despues ejecuta cada test
+  //@BeforeEach//Antes de cada test ejecuto esto
+  @BeforeAll
+  static void setUpAll() {//siempre
+	  System.out.println("@BeforeAll");
+  }
+  
+  @BeforeEach
+  void setUp() {//siempre y no es estatico
+	  System.out.println("@BeforeEach");
+  }
+  
+  @AfterEach
+  void tearDown() {//siempre y no es estatico
+	  System.out.println("@AfterEach");
+  }
+  
+  @AfterAll
+  static void tearDownAll() {//siempre
+	  System.out.println("@AfterAll");
+  }
+  
   @Test
+  @DisplayName("Prueba Demo")
   void testBasicSalaryWithValidSalary() {
+	System.out.println("Prueba Demo");
     basicDemo = new BasicDemo();
     String expectedDemo = "Prueba";
     basicDemo.setDemoString(expectedDemo);
     assertEquals(expectedDemo, basicDemo.getDemoString());
 
   }
-
+  @Test
+  void simpleTest() {
+	  System.out.println("simpleTest");
+	  assertEquals(true, true);
+  }
 }
