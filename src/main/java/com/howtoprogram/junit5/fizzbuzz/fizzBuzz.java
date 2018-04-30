9a53a1d57b75e3928c71dc70a30b166cfc669f44
uriel270195 <uriel_270195@hotmail.com>
2018-04-30 02:02:03 -0500

    pruebas unitarias

--- src/main/java/com/howtoprogram/junit5/fizzbuzz/fizzBuzz.java
+++ src/main/java/com/howtoprogram/junit5/fizzbuzz/fizzBuzz.java
@@ -1,32 +1,33 @@
 package com.howtoprogram.junit5.fizzbuzz;
 
-import io.reactivex.Flowable;
+import io.reactivex.Observable;
 
 public class fizzBuzz {
 	static String arrFizzBuzz[];
-	
+	static int pos=0;
 	public static String[] fb(int num1, int num2) {
 		// TODO Auto-generated method stub
 		if (!validate(num1,num2)) {
 			return new String[] {"Error"};
 		}
-		arrFizzBuzz=new String[num2];
-		int length=arrFizzBuzz.length;
-		int pos=0;
-		for (int x = 1; x <= length; x++) {
+		arrFizzBuzz=new String[num2-(num1)+1];
+		Observable.range(num1, num2).map(x -> {
 			arrFizzBuzz[pos]="";
-            if (x % 3 == 0) {
+			if (x % 3 == 0 && x % 5 == 0) {
+            	arrFizzBuzz[pos] = "FizzBuzz";
+            }else if (x % 3 == 0) {
             	arrFizzBuzz[pos] = arrFizzBuzz[pos]+ "Fizz";
-            }
-            if (x % 5 == 0) {
+            }else if (x % 5 == 0) {
             	arrFizzBuzz[pos] = arrFizzBuzz[pos] +"Buzz";
-            }
-            if (x % 3 != 0 && x % 5 != 0) {
+            }else {
             	arrFizzBuzz[pos] = x+"";
             }
-            System.out.println(arrFizzBuzz[pos]);
+            //System.out.println(arrFizzBuzz[pos]);
             pos++;
-		}
+            return (pos)-1;
+		}).subscribe(x ->{
+			System.out.println(arrFizzBuzz[x]);
+		});
 		return arrFizzBuzz;
 	}
 
